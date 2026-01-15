70ee6938c9ac7cfb89dec210aaaaa7c0ec4fad96
Mike <michaelpineau89@gmail.com>
2026-01-15 19:45:28 +0000

    fix: removed impersonate account

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 provider "google" {
   project                     = "mike-personal-portfolio "
   region                      = "us-central1"
-  impersonate_service_account = "terraform-deployer@mike-personal-portfolio.iam.gserviceaccount.com"
 }
