90a826573df30e2c9ff8643d6e9829c7ffadf9ff
Mike <michaelpineau89@gmail.com>
2026-01-15 21:50:28 +0000

    feat: added more terraform setup configs

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  # This empty block tells Terraform: "Expect to store state in GCS"
+  # We will fill in the bucket name via the command line (Partial Configuration)
+  backend "gcs" {}
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "google" {
   project = "mike-personal-portfolio"
   region  = "us-central1"
