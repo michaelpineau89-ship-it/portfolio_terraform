5da804b76719568a9159e3ce1da98d50b5f3ed7b
Michael Pineau <michael.pineau89@gmail.com>
2026-02-25 18:02:36 -0500

    feat: updated the dataflow args

--- flash-crash-detector.tf
+++ flash-crash-detector.tf
@@ -96,7 +96,6 @@ resource "google_dataflow_flex_template_job" "flash_crash_job" {
 
   # Parameters to pass to your pipeline.py
   parameters = {
-    worker_region       = "us-east1"
     worker_zone         = "us-east1-b"
     input_subscription = google_pubsub_subscription.stock_ticks_sub.id
     output_table       = "${var.project_id}:flash_crash_data.crashes"
