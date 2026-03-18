48e9ab421cedc9195d53d0e5088750450af39716
Michael Pineau <michael.pineau89@gmail.com>
2026-03-18 07:18:35 -0400

    feat: updated region

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "project_id" {
 variable "region" {
   description = "Default GCP Region"
   type        = string
-  default     = "us-east1"
+  default     = "us-central1"
 }
 
 variable "region_dataflow" {
