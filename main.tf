45bb8e2e363b77b85bc162706769e7b8379be5a9
Mike Pineau <mikepineau@Mikes-MacBook-Air.local>
2026-04-10 15:53:51 -0400

    fix: declare google-beta provider in required_providers to match lock file v7

--- main.tf
+++ main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 7.0"
+    }
   }
 }
 
